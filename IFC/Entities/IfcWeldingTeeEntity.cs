cb7ce3620d8c30ba825010c55e9d8b592ef1cfce
Glebfra <gleb.frantsuzov@yandex.ru>
2024-11-14 17:18:14 +0400

    + ifc welding tee entity

---
+++ IFC/Entities/IfcWeldingTeeEntity.cs
@@ -0,0 +1,14 @@
+﻿using IFC_Converter.Math;
+using IFC_Converter.Start.Entities;
+
+namespace IFC_Converter.IFC.Entities;
+
+public class IfcWeldingTeeEntity
+{
+    private StartWeldingTeeEntity _teeEntity;
+
+    public IfcWeldingTeeEntity(StartWeldingTeeEntity teeEntity)
+    {
+        _teeEntity = teeEntity;
+    }
+}
