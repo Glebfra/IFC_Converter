1619abc98ee604728571bf3737f7cce1159bc374
Glebfra <gleb.frantsuzov@yandex.ru>
2025-01-20 15:31:31 +0400

    - deleted unused code

--- IFC/Tools/IfcSegment.cs
+++ IFC/Tools/IfcSegment.cs
@@ -9,17 +9,6 @@ namespace IFC_Converter.IFC.Tools;
 
 public static class IfcSegment
 {
-    public static IfcPipeSegment CreatePipeSegment(IModel model, string name, IfcLocalPlacement placement, IfcProductRepresentation representation)
-    {
-        return model.Instances.New<IfcPipeSegment>(p =>
-        {
-            p.Name = name;
-            p.PredefinedType = IfcPipeSegmentTypeEnum.FLEXIBLESEGMENT;
-            p.ObjectPlacement = placement;
-            p.Representation = representation;
-        });
-    }
-
     public static IfcDistributionPort CreatePort(IModel model, string name, string description, IfcLocalPlacement placement)
     {
         return model.Instances.New<IfcDistributionPort>(p =>
