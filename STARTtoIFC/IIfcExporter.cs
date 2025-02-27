08a793b3c8be5bfaa150f7dc1d88d76d2fe0d3ad
Илья (офис) <ilyanicha@mail.ru>
2025-02-27 13:31:16 +0300

    Добавил проект для подключения к старту

---
+++ STARTtoIFC/IIfcExporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace STARTtoIFC
+{
+    [Guid("2649402A-A530-4CB8-A0C3-22661C463823")]
+    public interface IIfcExporter
+    {
+        int Export(object startDocument, int languageId);
+
+        int Test();
+    }
+}
---
+++ STARTtoIFC/IfcExporter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace STARTtoIFC
+{
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [Guid("8023137E-9E17-41A7-96AE-7D7688F1EC14")]
+    public class IfcExporter : IIfcExporter
+    {
+        public int Test()
+        {
+            MessageBox.Show("DLL is connected.");
+
+            return 1;
+        }
+
+        public int Export(object startDocument, int languageId)
+        {
+            MessageBox.Show("Здесь будет окно эксопрта в IFC");
+
+            if (startDocument != null)
+                Marshal.ReleaseComObject(startDocument);
+            startDocument = null;
+
+            return (int)ConversionResult.Success;
+        }
+
+        private void Localize(int languageId)
+        {
+            var ci = new System.Globalization.CultureInfo(languageId);
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+    }
+}
