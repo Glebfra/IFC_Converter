974e2f060cfd304e0981458b276560d83f87dc78
Glebfra <gleb.frantsuzov@yandex.ru>
2025-05-12 14:52:21 +0500

    [fix] restructured the ifc tools classes

---
+++ IFC/Tools/IfcGeometry.Primitives.cs
@@ -0,0 +1,43 @@
+﻿using Xbim.Common;
+using Xbim.Common.Geometry;
+using Xbim.Ifc4.GeometricModelResource;
+using Xbim.Ifc4.GeometryResource;
+using Xbim.Ifc4.ProfileResource;
+
+namespace IFC.Tools
+{
+    public static partial class IfcGeometry
+    {
+        public static IfcPlane CreatePlane(IModel model, XbimVector3D coordinates, XbimVector3D direction)
+        {
+            return model.Instances.New<IfcPlane>(plane =>
+            {
+                plane.Position = IfcAxis.CreateAxis2Placement3D(model, coordinates, direction);
+            });
+        }
+
+        public static IfcExtrudedAreaSolid CreateRectangle(IModel model, double xDim, double yDim, double zDim, XbimVector3D coordinates)
+        {
+            IfcRectangleProfileDef rectangleProfileDef = CreateRectangleProfileDef(model, xDim, yDim);
+            return CreateExtrudedAreaSolid(model, rectangleProfileDef, zDim, coordinates);
+        }
+
+        public static IfcExtrudedAreaSolid CreateCylinder(IModel model, double radius, double zDim, XbimVector3D coordinates)
+        {
+            IfcCircleProfileDef circleProfileDef = CreateCircleProfileDef(model, radius);
+            return CreateExtrudedAreaSolid(model, circleProfileDef, zDim, coordinates);
+        }
+        
+        public static IfcExtrudedAreaSolid CreateRectangle(IModel model, double xDim, double yDim, double zDim, XbimVector3D coordinates, XbimVector3D axis, XbimVector3D refDirection)
+        {
+            IfcRectangleProfileDef rectangleProfileDef = CreateRectangleProfileDef(model, xDim, yDim);
+            return CreateExtrudedAreaSolid(model, rectangleProfileDef, zDim, coordinates, axis, refDirection);
+        }
+
+        public static IfcExtrudedAreaSolid CreateCylinder(IModel model, double radius, double zDim, XbimVector3D coordinates, XbimVector3D axis, XbimVector3D refDirection)
+        {
+            IfcCircleProfileDef circleProfileDef = CreateCircleProfileDef(model, radius);
+            return CreateExtrudedAreaSolid(model, circleProfileDef, zDim, coordinates, axis, refDirection);
+        }
+    }
+}
