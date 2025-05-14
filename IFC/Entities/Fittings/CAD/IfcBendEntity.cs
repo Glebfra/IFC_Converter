613a93366fc157a931f8167e6e1be2b61c23e750
Glebfra <gleb.frantsuzov@yandex.ru>
2025-05-14 12:41:08 +0500

    [feature] added nonstandard properties to expansion joint entity

--- IFC/Entities/Fittings/CAD/IfcBendEntity.cs
+++ IFC/Entities/Fittings/CAD/IfcBendEntity.cs
@@ -1,10 +1,10 @@
 ﻿using IFC.Entities.Abstract;
+using IFC.Extensions;
 using IFC.Tools;
 using Start.Entities.Fittings;
 using Xbim.Common;
 using Xbim.Common.Geometry;
 using Xbim.Ifc4.GeometricModelResource;
-using Xbim.Ifc4.GeometryResource;
 using Xbim.Ifc4.HvacDomain;
 using Xbim.Ifc4.Interfaces;
 using Xbim.Ifc4.Kernel;
@@ -26,10 +26,10 @@ namespace IFC.Entities.Fittings.CAD
         public override IfcProduct CreateAndAdd(IModel model)
         {
             IfcObjectPlacement objectPlacement = IfcAxis.CreatePointAndDirectionsObjectPlacement(model, ObjectMatrix3D);
-            
-            IfcCircle circle = IfcGeometry.CreateCircle(model, _BendRadius, XbimVector3D.Zero, new XbimVector3D(0, 1, 0), new XbimVector3D(1, 0, 0));
-            IfcTrimmedCurve bendCurve = IfcGeometry.CreateTrimmedCurve(model, circle, -Angle, 0);
-            IfcSweptDiskSolid pipeBend = IfcGeometry.CreateSweptDiskSolid(model, bendCurve, _PipeRadius);
+            IfcSweptDiskSolid pipeBend = IfcGeometry.CreateCircularBend(
+                model, _PipeRadius, _BendRadius, Angle,
+                XbimVector3D.Zero, VectorExtensions.Forward, VectorExtensions.Right
+            );
             IfcShapeRepresentation shapeRepresentation = IfcGeometry.CreateShapeRepresentation(model, pipeBend);
 
             IfcProductDefinitionShape shape = IfcGeometry.CreateProductDefinitionShape(model, shapeRepresentation);
