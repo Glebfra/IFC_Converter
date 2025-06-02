3e37c6ced9711daa1d60fdf2fe1e29f43df43365
Glebfra <gleb.frantsuzov@yandex.ru>
2025-06-02 13:38:56 +0500

    [fix] rigid hanger

---
+++ IFC/Entities/Abstract/Anchors/IfcAbstractRigidHangerEntity.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using IFC.Entities.Abstract.Segments;
+using IFC.Extensions;
+using IFC.Tools;
+using Start.Entities.Anchors;
+using Xbim.Common;
+using Xbim.Common.Geometry;
+using Xbim.Ifc4.GeometryResource;
+using Xbim.Ifc4.Interfaces;
+using Xbim.Ifc4.Kernel;
+using Xbim.Ifc4.RepresentationResource;
+using Xbim.Ifc4.SharedComponentElements;
+
+namespace IFC.Entities.Abstract.Anchors
+{
+    public abstract class IfcAbstractRigidHangerEntity : IfcAbstractNonFixedSupportEntity
+    {
+        public abstract int NumSegments { get; protected set; }
+        public abstract double Height { get; protected set; }
+
+        private readonly StartRigidHangerEntity _rigidHanger;
+        private IfcDiscreteAccessory? _discreteAccessory;
+        
+        protected IfcAbstractRigidHangerEntity(StartRigidHangerEntity rigidHanger, IfcNodeEntity nodeEntity, IfcAbstractSegmentEntity[] segmentEntities) 
+            : base(rigidHanger, nodeEntity, segmentEntities)
+        {
+            _rigidHanger = rigidHanger;
+        }
+        
+        public override IfcProduct CreateAndAdd(IModel model)
+        {
+            IfcObjectPlacement objectPlacement = IfcAxis.CreatePointAndDirectionsObjectPlacement(model, ObjectMatrix3D);
+
+            IEnumerable<IfcRepresentationItem> representationItems = CreateAnchor(model, -Diameter / 2 * VectorExtensions.Forward);
+            IfcShapeRepresentation shapeRepresentation = IfcVertexGeometry.CreateShapeRepresentation(model, representationItems);
+            IfcProductDefinitionShape shape = IfcGeometry.CreateProductDefinitionShape(model, shapeRepresentation);
+            ColourEntity(model, representationItems);
+            
+            _discreteAccessory = model.Instances.New<IfcDiscreteAccessory>(accessory =>
+            {
+                accessory.Name = _rigidHanger.Name;
+                accessory.Tag = Tag;
+                accessory.PredefinedType = IfcDiscreteAccessoryTypeEnum.ANCHORPLATE;
+                accessory.Representation = shape;
+                accessory.ObjectPlacement = objectPlacement.LocalPlacement;
+            });
+            AddProperties(model, _discreteAccessory);
+
+            return _discreteAccessory;
+        }
+
+        protected override IEnumerable<IfcRepresentationItem> CreateAnchorModel(IModel model, XbimVector3D displacement)
+        {
+            IfcRepresentationItem[] representationItems = new IfcRepresentationItem[3];
+
+            double rectangleXDim = Diameter;
+            double rectangleYDim = Diameter;
+            double rectangleHeight = Height / 20;
+            XbimVector3D rectangleCoordinates = displacement + (Height - rectangleHeight) * VectorExtensions.Forward;
+            representationItems[0] = IfcGeometry.CreateRectangle(model, rectangleXDim, rectangleYDim, rectangleHeight, rectangleCoordinates);
+
+            double stickRadius = Diameter / 10;
+            double stickHeight = Height / 3;
+            XbimVector3D stickCoordinates = rectangleCoordinates - stickHeight * VectorExtensions.Forward;
+            representationItems[1] = IfcGeometry.CreateCylinder(model, stickRadius, stickHeight, stickCoordinates);
+
+            XbimVector3D coneCoordinates = stickCoordinates;
+            XbimVector3D coneTopCoordinates = displacement + Diameter / 2 * VectorExtensions.Forward;
+            double coneRadius = Diameter / 4;
+            IfcCartesianPoint[] circle = IfcVertexGeometry.CreateCircle(model, coneRadius, coneCoordinates, NumSegments);
+            IfcCartesianPoint topPoint = IfcAxis.CreatePoint(model, coneTopCoordinates);
+            representationItems[2] = IfcVertexGeometry.CreateCone(model, circle, topPoint);
+
+            return representationItems;
+        }
+    }
+}
