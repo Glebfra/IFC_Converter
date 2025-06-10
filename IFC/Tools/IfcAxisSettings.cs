11c4619f17d41919e226cd896d85d2fe930ef939
Glebfra <gleb.frantsuzov@yandex.ru>
2025-06-10 19:05:33 +0500

    [feature] added triangulated and polygonal face sets

--- IFC/Tools/IfcAxisSettings.cs
+++ IFC/Tools/IfcAxisSettings.cs
@@ -11,25 +11,25 @@ namespace IFC.Tools
         
         public IfcAxisSettings(XbimVector3D xAxis)
         {
-            XAxis = xAxis;
-            YAxis = new XbimVector3D(xAxis.Y, xAxis.Z, xAxis.X);
-            ZAxis = XbimVector3D.CrossProduct(XAxis, YAxis);
+            XAxis = xAxis.Normalized();
+            YAxis = new XbimVector3D(xAxis.Y, xAxis.Z, xAxis.X).Normalized();
+            ZAxis = XbimVector3D.CrossProduct(XAxis, YAxis).Normalized();
             Origin = XbimVector3D.Zero;
         }
 
         public IfcAxisSettings(XbimVector3D xAxis, XbimVector3D yAxis)
         {
-            XAxis = xAxis;
-            YAxis = yAxis;
-            ZAxis = XbimVector3D.CrossProduct(XAxis, YAxis);
+            XAxis = xAxis.Normalized();
+            YAxis = yAxis.Normalized();
+            ZAxis = XbimVector3D.CrossProduct(XAxis, YAxis).Normalized();
             Origin = XbimVector3D.Zero;
         }
 
         public IfcAxisSettings(XbimVector3D origin, XbimVector3D xAxis, XbimVector3D yAxis)
         {
-            XAxis = xAxis;
-            YAxis = yAxis;
-            ZAxis = XbimVector3D.CrossProduct(XAxis, YAxis);
+            XAxis = xAxis.Normalized();
+            YAxis = yAxis.Normalized();
+            ZAxis = XbimVector3D.CrossProduct(XAxis, YAxis).Normalized();
             Origin = origin;
         }
     }
--- IFC/Tools/IfcVertexGeometry.ShapeTools.cs
+++ IFC/Tools/IfcVertexGeometry.ShapeTools.cs
@@ -1,12 +1,46 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using IFC.Extensions;
 using Xbim.Common;
+using Xbim.Common.Geometry;
+using Xbim.Ifc4.GeometricModelResource;
 using Xbim.Ifc4.GeometryResource;
+using Xbim.Ifc4.MeasureResource;
 using Xbim.Ifc4.TopologyResource;
 
 namespace IFC.Tools
 {
     public static partial class IfcVertexGeometry
     {
+        public static IfcTriangulatedFaceSet CreateTriangulatedFaceSet(IModel model, IfcCartesianPointList3D coordinates, int[][] vertices, XbimVector3D[] normals, bool closed)
+        {
+            IfcTriangulatedFaceSet triangulatedFaceSet = model.Instances.New<IfcTriangulatedFaceSet>();
+            triangulatedFaceSet.Coordinates = coordinates;
+            triangulatedFaceSet.Closed = closed;
+
+            int vertexIndex = 0;
+            triangulatedFaceSet.AddVertices(vertices, ref vertexIndex);
+            int normalIndex = 0;
+            triangulatedFaceSet.AddNormals(normals, ref normalIndex);
+
+            return triangulatedFaceSet;
+        }
+
+        public static IfcPolygonalFaceSet CreatePolygonalFaceSet(IModel model, IfcCartesianPointList3D coordinates)
+        {
+            IfcPolygonalFaceSet polygonalFaceSet = model.Instances.New<IfcPolygonalFaceSet>();
+            polygonalFaceSet.Coordinates = coordinates;
+
+            return polygonalFaceSet;
+        }
+        
+        public static IfcIndexedPolygonalFace CreateIndexedPolygonalFace(IModel model, int[] indices)
+        {
+            IfcIndexedPolygonalFace indexedFace = model.Instances.New<IfcIndexedPolygonalFace>();
+            indexedFace.CoordIndex.AddRange(indices.Cast<IfcPositiveInteger>());
+            return indexedFace;
+        }
+        
         public static IfcFace CreateRectangleFace(IModel model, IfcCartesianPoint p1, IfcCartesianPoint p2, IfcCartesianPoint p3, IfcCartesianPoint p4)
         {
             return CreatePolygonFace(model, new[] { p1, p2, p3, p4 });
