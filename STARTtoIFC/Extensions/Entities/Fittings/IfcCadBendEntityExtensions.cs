a32a74489616d52ec96b9ed3e6af31e7702261b1
glebf <gleb.frantsuzov@yandex.ru>
2025-08-28 14:48:47 +0500

    [feature] moved extensions methods to their extension class [feature] restructured import and export directories

--- STARTtoIFC/Extensions/Entities/Fittings/IfcCadBendEntityExtensions.cs
+++ STARTtoIFC/Extensions/Entities/Fittings/IfcCadBendEntityExtensions.cs
@@ -54,33 +54,9 @@ namespace STARTtoIFC.Extensions.Entities.Fittings
 
             Pset_Start? psetStart = ifcCadBendEntity.PropertySets.OfType<Pset_Start>().FirstOrDefault();
             if (psetStart != null)
-                UpdateStartEntityFromStartPset(ref startBendEntity, psetStart);
+                startBendEntity.UpdateFromStartPset(psetStart);
 
             return startBendEntity;
         }
-        
-        private static void UpdateStartEntityFromStartPset(ref StartBendEntity startBendEntity, Pset_Start psetStart)
-        {
-            Dictionary<string, string>? data = psetStart.Data;
-            
-            if (data.TryGetValue(nameof(startBendEntity.ManufacturingTechnologyEnum), out string manufacturingTechnology))
-                startBendEntity.ManufacturingTechnologyEnum = ManufacturingTechnologyExtensions.GetManufacturingTechnology(manufacturingTechnology);
-            if (data.TryGetValue(nameof(startBendEntity.MaterialName), out string materialName))
-                startBendEntity.MaterialName = materialName;
-            if (data.TryGetValue(nameof(startBendEntity.MillTolerance), out string millTolerance))
-                startBendEntity.MillTolerance = LengthProperty.CreateFromSi(GetPropertyValue(millTolerance));
-            if (data.TryGetValue(nameof(startBendEntity.WallThickness), out string wallThickness))
-                startBendEntity.WallThickness = LengthProperty.CreateFromSi(GetPropertyValue(wallThickness));
-            if (data.TryGetValue(nameof(startBendEntity.OvalizationCoefficient), out string ovalizationCoefficient))
-                startBendEntity.OvalizationCoefficient = FactorProperty.CreateFromSi(GetPropertyValue(ovalizationCoefficient));
-            if (data.TryGetValue(nameof(startBendEntity.NumberOfMilters), out string numberOfMilters))
-                startBendEntity.NumberOfMilters = NumberProperty.CreateFromSi((int)GetPropertyValue(numberOfMilters));
-            if (data.TryGetValue(nameof(startBendEntity.MillToleranceOutside), out string millToleranceOutside))
-                startBendEntity.MillToleranceOutside = LengthProperty.CreateFromSi(GetPropertyValue(millToleranceOutside));
-            if (data.TryGetValue(nameof(startBendEntity.Weight), out string weight))
-                startBendEntity.Weight = MassProperty.CreateFromSi(GetPropertyValue(weight));
-        }
-        
-        private static double GetPropertyValue(string rawValue) => Pset_StartExtensions.GetDoublePropertyValue(rawValue);
     }
 }
--- STARTtoIFC/Extensions/Entities/Segments/IfcPipeSegmentEntityExtensions.cs
+++ STARTtoIFC/Extensions/Entities/Segments/IfcPipeSegmentEntityExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using IFC.Entities;
 using IFC.Entities.Segments;
@@ -52,43 +51,9 @@ namespace STARTtoIFC.Extensions.Entities.Segments
 
             Pset_Start? psetStart = ifcPipeSegmentEntity.PropertySets.OfType<Pset_Start>().FirstOrDefault();
             if (psetStart != null) 
-                UpdateStartEntityFromStartPset(ref startPipeEntity, psetStart);
+                startPipeEntity.UpdateFromStartPset(psetStart);
 
             return startPipeEntity;
         }
-
-        private static void UpdateStartEntityFromStartPset(ref StartPipeEntity startPipeEntity, Pset_Start psetStart)
-        {
-            Dictionary<string, string> data = psetStart.Data;
-
-            if (data.TryGetValue(nameof(startPipeEntity.ManufacturingTechnologyEnum), out string manufacturingTechnology))
-                startPipeEntity.ManufacturingTechnologyEnum = ManufacturingTechnologyExtensions.GetManufacturingTechnology(manufacturingTechnology);
-            if (data.TryGetValue(nameof(startPipeEntity.MaterialName), out string materialName))
-                startPipeEntity.MaterialName = materialName;
-            if (data.TryGetValue(nameof(startPipeEntity.MillTolerance), out string millTolerance))
-                startPipeEntity.MillTolerance = LengthProperty.CreateFromSi(GetPropertyValue(millTolerance));
-            if (data.TryGetValue(nameof(startPipeEntity.CorrosionAllowance), out string corrosionAllowance))
-                startPipeEntity.CorrosionAllowance = LengthProperty.CreateFromSi(GetPropertyValue(corrosionAllowance));
-            if (data.TryGetValue(nameof(startPipeEntity.PipeUnitWeight), out string pipeUnitWeight))
-                startPipeEntity.PipeUnitWeight = MassUnitProperty.CreateFromSi(GetPropertyValue(pipeUnitWeight));
-            if (data.TryGetValue(nameof(startPipeEntity.InsulationUnitWeight), out string insulationWeight))
-                startPipeEntity.InsulationUnitWeight = MassUnitProperty.CreateFromSi(GetPropertyValue(insulationWeight));
-            if (data.TryGetValue(nameof(startPipeEntity.ProductUnitWeight), out string productUnitWeight))
-                startPipeEntity.ProductUnitWeight = MassUnitProperty.CreateFromSi(GetPropertyValue(productUnitWeight));
-            if (data.TryGetValue(nameof(startPipeEntity.LongitudinalWeldJointFactor), out string longitudinalWeldJointFactor))
-                startPipeEntity.LongitudinalWeldJointFactor = FactorProperty.CreateFromSi(GetPropertyValue(longitudinalWeldJointFactor));
-            if (data.TryGetValue(nameof(startPipeEntity.StrengthFactorOfTheTraverseWeld), out string strengthFactorOfTraverseWeld))
-                startPipeEntity.StrengthFactorOfTheTraverseWeld = FactorProperty.CreateFromSi(GetPropertyValue(strengthFactorOfTraverseWeld));
-            if (data.TryGetValue(nameof(startPipeEntity.AdditionalWeightLoad), out string additionalWeightLoad))
-                startPipeEntity.AdditionalWeightLoad = MassUnitProperty.CreateFromSi(GetPropertyValue(additionalWeightLoad));
-            if (data.TryGetValue(nameof(startPipeEntity.AdditionalWeightLoadAlongTheXAxis), out string additionalWeightLoadAlongTheXAxis))
-                startPipeEntity.AdditionalWeightLoadAlongTheXAxis = MassUnitProperty.CreateFromSi(GetPropertyValue(additionalWeightLoadAlongTheXAxis));
-            if (data.TryGetValue(nameof(startPipeEntity.AdditionalWeightLoadAlongTheYAxis), out string additionalWeightLoadAlongTheYAxis))
-                startPipeEntity.AdditionalWeightLoadAlongTheYAxis = MassUnitProperty.CreateFromSi(GetPropertyValue(additionalWeightLoadAlongTheYAxis));
-            if (data.TryGetValue(nameof(startPipeEntity.AdditionalWeightLoadAlongTheZAxis), out string additionalWeightLoadAlongTheZAxis))
-                startPipeEntity.AdditionalWeightLoadAlongTheZAxis = MassUnitProperty.CreateFromSi(GetPropertyValue(additionalWeightLoadAlongTheZAxis));
-        }
-        
-        private static double GetPropertyValue(string rawValue) => Pset_StartExtensions.GetDoublePropertyValue(rawValue);
     }
 }
