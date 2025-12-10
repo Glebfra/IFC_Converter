1bceae23dfc1920601ffe64390c6526de6b52d0c
glebf <gleb.frantsuzov@yandex.ru>
2025-12-10 12:16:20 +0400

    [fix] added filepaths to ifc import methods

--- IFCConverter/IfcConverter.cs
+++ IFCConverter/IfcConverter.cs
@@ -85,71 +85,75 @@ namespace IFCConverter
         }
 
         [STAThread]
-        public int ImportFromFileImport(object startAutoServerObject, int languageId, string tempFileName)
+        public int ImportFromFileImport(object startAutoServerObject, int languageId, string startTempFileName)
         {
-            using (StartAutoServer startAutoServer = new StartAutoServer(startAutoServerObject))
+            return ImportFromFileOpen(startAutoServerObject, languageId, startTempFileName);
+        }
+
+        [STAThread]
+        public int ImportFromFileOpen(object startAutoServerObject, int languageId, string startTempFileName, string ifcFileName="")
+        {
+            Logger logger = Logger.GetInstance();
+            Localize(languageId);
+
+            using (StartAutoServer autoServer = new StartAutoServer(startAutoServerObject))
             {
-                object startDocumentRaw = startAutoServer.LoadStartDocumentRaw(0x4, tempFileName);
-                return ImportFromFileOpen(startDocumentRaw, languageId);
+                try
+                {
+                    StartDocument startDocument = autoServer.LoadStartDocument(0x2, startTempFileName);
+                    ImportDataContainer importDataContainer = new ImportDataContainer() { InputFilePath = ifcFileName };
+                    return Import(startDocument, importDataContainer);
+                }
+                catch (Exception e)
+                {
+                    return (int)ConversionResult.Fail;
+                }
             }
         }
 
         [STAThread]
-        public int ImportFromFileOpen(object startDocumentObject, int languageId)
+        private int Import(StartDocument startDocument, ImportDataContainer importDataContainer)
         {
+            Application.EnableVisualStyles();
+            Logger logger = Logger.GetInstance();
+            
+            DialogResult dialogResult;
+            using (ImportWindowForm importWindowForm = new ImportWindowForm(importDataContainer))
+            {
+                dialogResult = importWindowForm.ShowDialog();
+            }
+                
+            if (dialogResult == DialogResult.Cancel)
+            {
+                return (int)ConversionResult.Canceled;
+            }
+            
             try
             {
-                Application.EnableVisualStyles();
-                Logger logger = Logger.GetInstance();
-                
-                Localize(languageId);
-                
-                StartDocument startDocument = new StartDocument(startDocumentObject);
-                ImportDataContainer importDataContainer = new ImportDataContainer();
-
-                DialogResult dialogResult;
-                using (ImportWindowForm importWindowForm = new ImportWindowForm(importDataContainer))
-                {
-                    dialogResult = importWindowForm.ShowDialog();
-                }
-                
-                if (dialogResult == DialogResult.Cancel)
-                {
-                    return (int)ConversionResult.Canceled;
-                }
-
-                try
-                {
-                    logger.Info($"Converting start at {DateTime.Now}");
-                    StartGenerator startGenerator = new StartGenerator(importDataContainer);
-                    startGenerator.Convert(startDocument);
-                    logger.Info($"Convert is successfully ended at {DateTime.Now}");
+                logger.Info($"Converting start at {DateTime.Now}");
+                StartGenerator startGenerator = new StartGenerator(importDataContainer);
+                startGenerator.Convert(startDocument);
+                logger.Info($"Convert is successfully ended at {DateTime.Now}");
                     
-                    #if DEBUG
-                    logger.SaveAs(importDataContainer.InputFilePath + ".log");
-                    #else
-                    if (logger.HasErrors())
-                    {
-                        logger.SaveAs(importDataContainer.InputFilePath + ".log");
-                    }
-                    else
-                    {
-                        logger.Flush();
-                    }
-                    #endif
-                    
-                    return (int)ConversionResult.Success;
-                }
-                catch (Exception e)
+                #if DEBUG
+                logger.SaveAs(importDataContainer.InputFilePath + ".log");
+                #else
+                if (logger.HasErrors())
                 {
-                    logger.Error(e.ToString());
                     logger.SaveAs(importDataContainer.InputFilePath + ".log");
-                    return (int)ConversionResult.Fail;
                 }
+                else
+                {
+                    logger.Flush();
+                }
+                #endif
+                    
+                return (int)ConversionResult.Success;
             }
             catch (Exception e)
             {
-                MessageBox.Show(e.ToString());
+                logger.Error(e.ToString());
+                logger.SaveAs(importDataContainer.InputFilePath + ".log");
                 return (int)ConversionResult.Fail;
             }
         }
