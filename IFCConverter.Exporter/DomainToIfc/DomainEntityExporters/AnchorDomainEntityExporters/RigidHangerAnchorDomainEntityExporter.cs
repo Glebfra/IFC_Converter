0bf80a61425f6643752e2b47fb120513a0c30807
glebf <gleb.frantsuzov@yandex.ru>
2026-08-31 12:51:35 +0400

    [feature] added resting support and rigid hanger anchors export

---
+++ IFCConverter.Exporter/DomainToIfc/DomainEntityExporters/AnchorDomainEntityExporters/RigidHangerAnchorDomainEntityExporter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using IFCConverter.Domain.Entities;
+using IFCConverter.IFC.API;
+using IFCConverter.IFC.Builders.Elements;
+using IFCConverter.IFC.Geometries;
+using IFCConverter.IFC.Interfaces;
+using IFCConverter.Start.API;
+using IFCConverter.Utils.Mathematics;
+using MathNet.Numerics.LinearAlgebra;
+using Xbim.Common;
+using Xbim.Ifc4.Interfaces;
+using Xbim.Ifc4.SharedComponentElements;
+using VectorExtensions = IFCConverter.Utils.Mathematics.VectorExtensions;
+using MatrixExtensions = IFCConverter.Utils.Mathematics.MatrixExtensions;
+
+namespace IFCConverter.Exporter.DomainToIfc.DomainEntityExporters.AnchorDomainEntityExporters
+{
+    internal sealed class RigidHangerAnchorDomainEntityExporter : IAnchorDomainEntityExporter
+    {
+        public bool CanExport(Anchor anchor)
+        {
+            if (!Enum.TryParse(anchor.Metadata.Type, out StartElementTypeEnum type))
+                return false;
+
+            return type == StartElementTypeEnum.RIGID_HANGER;
+        }
+
+        public void Export(Anchor anchor, IModel model, ExportContext context)
+        {
+            Matrix<double> segmentMatrix = (Matrix<double>)anchor.Metadata.Meta["SegmentMatrix"];
+            double diameter = anchor.Port.Metadata.Diameter;
+            
+            bool isDoubleSided = segmentMatrix.GetZ().IsParallel(VectorExtensions.Z);
+            Vector<double> position, doubleSidedDisplacement;
+            if (isDoubleSided)
+            {
+                position = VectorExtensions.Zero;
+                doubleSidedDisplacement = segmentMatrix.GetX() * diameter;
+            }
+            else
+            {
+                double displacement = MathExtensions.CalculateAnchorDisplacement(segmentMatrix, diameter);
+                position = displacement * VectorExtensions.Z;
+                doubleSidedDisplacement = VectorExtensions.Zero;
+            }
+            
+            IIfcGeometry geometry = RigidHangerAnchorGeometry.CreateGeometry(model, new RigidHangerAnchorGeometryProperties()
+            {
+                Position = position,
+                Direction = -VectorExtensions.Z,
+                Diameter = diameter,
+                IsDoubleSided = isDoubleSided,
+                DoubleSidedDisplacement = doubleSidedDisplacement
+            });
+            geometry.AssignColor(Color.FromHEX(anchor.Metadata.Color));
+            
+            Matrix<double> placement = MatrixExtensions.CreateTransition(anchor.Position);
+            IIfcDiscreteAccessoryBuilder<IIfcDiscreteAccessory> builder =
+                new IfcDiscreteAccessoryBuilder<IfcDiscreteAccessory>(anchor.Metadata.Name, anchor.Metadata.Type, IfcDiscreteAccessoryTypeEnum.USERDEFINED);
+            builder.AssignGeometry(geometry);
+            builder.CreateObjectPlacement(model, placement);
+
+            IIfcProduct instance = builder.CreateInstance(model);
+            context.Register(anchor, instance);
+        }
+    }
+}
